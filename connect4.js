daf486c351a0e989d7f68b2933bcc1a129847fe2
Jassim Vailoces <jassimv88@yahoo.com>
2023-10-18 22:51:16 -0400

    adding notes in js

--- connect4.js
+++ connect4.js
@@ -1,7 +1,15 @@
-define variables
-player Red
-player Yellow
+// define variables
+// player Red
+// player Yellow
 
-rows = 6;
-columns = 7;
+// rows = 6;
+// columns = 7;
 
+// function to play
+
+
+// function to check winner
+// horizontal
+// vertical
+// diagonal left
+// diagonal right
